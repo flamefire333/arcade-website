ccfb320f3388dbc7b57fafd3b353e1e5094608c2
flamefire333 <flamefire333@gmail.com>
2021-03-19 22:12:50 -0500

    Check Database for characters

--- backend/character/character.go
+++ backend/character/character.go
@@ -1,5 +1,10 @@
 package character
 
+import (
+	"arcade-website/database"
+	"log"
+)
+
 type Character struct {
 	ID      int
 	Name    string
@@ -80,25 +85,23 @@ var AllCharacters []Character = []Character{
 }
 
 func SetupCharacters() {
-	return
-	/*
-		Conn, err := database.GetDBConn()
-		defer Conn.Close()
-		if err != nil {
-			log.Printf("Getting DB for Character Selected failed %+v\n", err)
-			return
-		}
-		rows, err := Conn.Query("SELECT id, name, avatar, group_id FROM characters")
-		if err != nil {
-			log.Printf("Character SELECT failed %+v\n", err)
-			return
-		}
-		characters := make([]Character, 0)
-		for rows.Next() {
-			c := Character{}
-			rows.Scan(&c.ID, &c.Name, &c.Avatar, &c.GroupID)
-			characters = append(characters, c)
-		}
-		AllCharacters = characters
-		log.Printf("Characters Loaded: %+v\n", AllCharacters)*/
+	Conn, err := database.GetDBConn()
+	defer Conn.Close()
+	if err != nil {
+		log.Printf("Getting DB for Character Selected failed %+v\n", err)
+		return
+	}
+	rows, err := Conn.Query("SELECT id, name, avatar, group_id FROM characters")
+	if err != nil {
+		log.Printf("Character SELECT failed %+v\n", err)
+		return
+	}
+	characters := make([]Character, 0)
+	for rows.Next() {
+		c := Character{}
+		rows.Scan(&c.ID, &c.Name, &c.Avatar, &c.GroupID)
+		characters = append(characters, c)
+	}
+	AllCharacters = characters
+	log.Printf("Characters Loaded: %+v\n", AllCharacters)
 }
